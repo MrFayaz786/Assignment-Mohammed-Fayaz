ba4f8771fae95626fb5efceb951908066b6d5902
MrFayaz786 <mohammedfayaz2003@gmail.com>
2026-03-04 15:41:34 +0530

    Task

--- Assignment-2/Task2.java
+++ Assignment-2/Task2.java
@@ -2,15 +2,16 @@ class AccountInfo{
 
 double balance;
 
-public void WithDraw(double amt){
+void WithDraw(double amt){
 if(amt <= balance){
-   balance-=amt;
+   balance = balance-amt;
    double taken = amt;
    System.out.println("Successfully Withdrawn :"+taken);
 }else{
    System.out.println("Insufficient Balance");
 }
 }
+
 }
 public class Task2 {
     
@@ -18,7 +19,7 @@ public class Task2 {
     AccountInfo c1 = new AccountInfo();
     c1.balance= 5000.00;
     c1.WithDraw(500);
-    System.out.println(c1.balance);
+    System.out.println("Balance : "+c1.balance);
 
   }  
 }
